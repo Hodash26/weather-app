1b7ef28bb7a1c528fcd6e7e30849ab0ece439354
florent <florent.bailly62400@gmail.com>
2019-09-19 14:52:22 +0200

    first commitweather-app

---
+++ src/components/Weather.vue
@@ -0,0 +1,158 @@
+<template>
+    <div class="weather">
+        <h2>
+            Météo du {{ date | formatDate }}
+        </h2>
+        <div class="wrap">
+            <div class="infos" v-if="infos">
+                <p>Maintenant</p>
+                <p class="city">
+                    {{infos.city.name}}
+                </p>
+                <p class="cloud">
+                    <img  v-bind:src="changeimage0" alt="image du temps">
+                </p>
+                <p class="temp">
+                    Temperature : {{ infos.list[0].main.temp}}°
+                </p>
+                <p class="humidity">
+                    Taux d'humidité: {{infos.list[0].main.humidity}}%
+                </p>
+                <p class="wind">
+                    Vitesse du vent : {{infos.list[0].wind.speed}}km/h
+                </p>
+            </div>
+    
+            <div class="infos" v-if="infos">
+                <p>Demain</p>
+                <p class="city">
+                    {{infos.city.name}}
+                </p>
+                <p class="cloud">
+                    <img  v-bind:src="changeimage0" alt="image du temps">
+                </p>
+                <p class="temp">
+                    Temperature : {{ infos.list[7].main.temp}}°
+                </p>
+                <p class="humidity">
+                    Taux d'humidité: {{infos.list[7].main.humidity}}%
+                </p>
+                <p class="wind">
+                    Vitesse du vent : {{infos.list[7].wind.speed}}km/h
+                </p>
+            </div>
+    
+            <div class="infos" v-if="infos">
+                <p>Après-demain</p>
+                <p class="city">
+                    {{infos.city.name}}
+                </p>
+                <p class="cloud">
+                    <img  v-bind:src="changeimage2" alt="image du temps">
+                </p>
+                <p class="temp">
+                    Temperature : {{ infos.list[15].main.temp}}°
+                </p>
+                <p class="humidity">
+                    Taux d'humidité: {{infos.list[15].main.humidity}}%
+                </p>
+                <p class="wind">
+                    Vitesse du vent : {{infos.list[15].wind.speed}}km/h
+                </p>
+            </div>
+    
+    
+            <div class="infos" v-if="infos">
+                <p>Dans 3 jours</p>
+                <p class="city">
+                    {{infos.city.name}}
+                </p>
+                <p class="cloud">
+                    <img  v-bind:src="changeimage2" alt="image du temps">
+                </p>
+                <p class="temp">
+                    Temperature : {{ infos.list[23].main.temp}}°
+                </p>
+                <p class="humidity">
+                    Taux d'humidité: {{infos.list[23].main.humidity}}%
+                </p>
+                <p class="wind">
+                    Vitesse du vent : {{infos.list[23].wind.speed}}km/h
+                </p>
+            </div>
+        </div>
+        </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+
+export default {
+    name: "Weather",
+    data: function()
+    {
+        return {
+            infos: null,
+            date: new Date()
+        }
+    },
+    computed: {
+        changeimage0: function() {
+            return this.infos ? 'https://openweathermap.org/img/wn/'+this.infos.list[0].weather[0].icon+'@2x.png' : '';
+        },
+        changeimage1: function() {
+            return this.infos ? 'https://openweathermap.org/img/wn/'+this.infos.list[7].weather[0].icon+'@2x.png' : "";
+        },
+        changeimage2: function() {
+            return this.infos ? 'https://openweathermap.org/img/wn/'+this.infos.list[15].weather[0].icon+'@2x.png' : "";
+        },
+        changeimage3: function() {
+            return this.infos ? 'https://openweathermap.org/img/wn/'+this.infos.list[23].weather[0].icon+'@2x.png': "";
+        }
+    },
+    created() {
+        axios
+            .get("https://api.openweathermap.org/data/2.5/forecast?q=Essars&units=metric&APPID=8ce918844b53476fc2c0fae219e82d75")
+            .then(response => (this.infos = response.data))
+    }
+}
+</script>
+
+<style>
+    html {
+        background-image: url('../assets/background1.jpeg');
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+
+    h2 {
+        font-size: 40px;
+        text-align: center;
+    }
+
+    .city {
+        font-size: 24px;
+    }
+
+    .infos {
+        text-align:center;
+        color: black;
+        border: 2px solid black;
+        font-size: 19px;
+        width: 40%;
+        margin: 15px;
+        }
+
+    .wrap {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        width: 100%;
+    }
+
+    img {
+        height: 150px;
+        width: 150px;
+    }
+</style>
